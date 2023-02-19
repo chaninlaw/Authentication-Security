163ea085bf2cb3833a8ca6bef328c9ab449f6b20
chaaninn <chaninlawlert@gmail.com>
2023-02-19 22:19:45 +0700

    Level 3 - Hash Password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrpyt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -27,7 +27,6 @@ const userSchema = new mongoose.Schema({
 
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrpyt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +42,7 @@ app.route("/login")
     })
     .post(function (req, res) {
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
 
         User.findOne(
             { email: username },
@@ -68,7 +67,7 @@ app.route("/register")
     .post(function (req, res) {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
         newUser.save(function (err) {
             if (!err) {
