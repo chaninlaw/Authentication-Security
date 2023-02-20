168c574a9111c226694e07ce8ea6c9a511bceb0e
chaaninn <chaninlawlert@gmail.com>
2023-02-20 17:45:22 +0700

    Level 5 - Using Passport.js to add cookies and sessions

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { use } = require("passport");
 
 const app = express();
 
-const saltRounds = 10;
 
 app.use(express.static("public"));
 
@@ -17,8 +18,17 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/userDB")
+mongoose.connect("mongodb://localhost:27017/userDB");
 
 
 const userSchema = new mongoose.Schema({
@@ -26,12 +36,17 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const secret = process.env.SECRET;
 
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -42,26 +57,21 @@ app.route("/login")
         res.render("login");
     })
     .post(function (req, res) {
-        const username = req.body.username;
-        const password = req.body.password;
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
 
-        User.findOne(
-            { email: username },
-            function (err, foundUser) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    if (foundUser) {
+        req.login(user, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                });
+            }
+        });
 
-                        bcrypt.compare(password, foundUser.password, function (err, result) {
-                            if (result === true) {
-                                res.render("secrets");
-                            }
-                        });
-
-                    }
-                }
-            });
     });
 
 app.route("/register")
@@ -70,21 +80,37 @@ app.route("/register")
     })
     .post(function (req, res) {
 
-        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            });
-            newUser.save(function (err) {
-                if (!err) {
-                    res.render("secrets");
-                } else {
-                    console.log(err);
-                }
-            });
+        User.register({ username: req.body.username }, req.body.password, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                });
+            }
         });
 
+    });
 
+app.route("/secrets")
+    .get(function (req, res) {
+        if (req.isAuthenticated()) {
+            res.render("secrets");
+        } else {
+            res.redirect("/login");
+        }
+    });
+
+app.route("/logout")
+    .get(function (req, res) {
+        req.logout(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/");
+            }
+        });
     });
 
 
